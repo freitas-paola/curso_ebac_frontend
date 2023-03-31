5119b56eab49bc0be56ad9348ed47be0eca4e61d
Paola Freitas <pahnetone@Paolas-MacBook-Air.local>
2023-03-31 07:16:04 -0300

    new changes added

---
+++ main.js
@@ -0,0 +1,17 @@
+$("form").on("submit", function (e) {
+  e.preventDefault();
+
+  const tarefa = $("#input-tarefa").val();
+
+  const novaTarefa = $(
+    `<li><ion-icon name="checkmark"></ion-icon>${tarefa}</li>`
+  );
+
+  novaTarefa.appendTo("ul");
+
+  $("#input-tarefa").val("");
+
+  $(novaTarefa).click(function () {
+    $(this).addClass("item-completo");
+  });
+});
