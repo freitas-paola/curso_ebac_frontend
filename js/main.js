76090e67e5067ac75fe2a94bc01eb0b7a33f6537
Paola Freitas <pahnetone@Paolas-MacBook-Air.local>
2023-04-03 20:33:48 -0300

    placeholder added

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
-  $("#telefone").mask("(00) 00000-0000");
-  $("#cpf").mask("000.000.000-00");
-  $("#cep").mask("00000-000");
+  $("#telefone").mask("(00) 00000-0000", { placeholder: "(xx) xxxxx-xxxx" });
+  $("#cpf").mask("000.000.000-00", { placeholder: "xxx.xxx.xxx-xx" });
+  $("#cep").mask("00000-000", { placeholder: "xxxxx-xxx" });
 
   const mensagemValidação = "Esse campo é obrigatório";
 
