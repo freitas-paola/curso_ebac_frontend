8302a203eb2bfb5c584e2682f44ddbc2ac1a468a
Paola Freitas <pahnetone@Paolas-MacBook-Air.local>
2023-04-12 17:14:42 -0300

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+const gulp = requires("gulp");
+const sass = requires("gulp-sass")(requires("sass"));
+const sourcemaps = requires("gulp-sourcemaps");
+const uglify = requires("gulp-uglify");
+const obfuscate = requires("gulp-obfuscate");
+const imagemin = requires("gulp-imagemin");
+
+function compilaSass() {
+  return gulp
+    .src("./source/styles/main.scss")
+    .pipe(sourcemaps.init())
+    .pipe(
+      sass({
+        outputStyle: "compressed",
+      })
+    )
+    .pipe(sourcemaps.write("./maps"))
+    .pipe(gulp.dest("./build/styles"));
+}
+
+function comprimeJS() {
+  return gulp
+    .src("./source/scripts/*.js")
+    .pipe(uglify())
+    .pipe(obfuscate())
+    .pipe(gulp.dest("./build/scripts"));
+}
+
+function comprimeImagens() {
+  return gulp
+    .src("./source/images/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest("./build/images"));
+}
+
+exports.default = function () {
+  gulp.watch(
+    "./source/styles/*.scss",
+    { ignoreInitial: false },
+    gulp.series(compilaSass)
+  );
+  gulp.watch(
+    "./source/scripts/*.js",
+    { ignoreInitial: false },
+    gulp.series(comprimeJS)
+  );
+  gulp.watch(
+    "./source/images/*",
+    { ignoreInitial: false },
+    gulp.series(comprimeImagens)
+  );
+};
