9fd5f9d3eb3f6ef7c6dd4c4bb90e9239e107ecbb
Paola Freitas <pahnetone@Paolas-MacBook-Air.local>
2023-05-05 19:30:51 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,21 @@
+class Aluno {
+  constructor(nome, nota) {
+    this.nome = nome;
+    this.nota = nota;
+  }
+}
+
+const Pedro = new Aluno("Pedro", 10);
+const Adam = new Aluno("Adam", 9);
+const Tim = new Aluno("Tim", 2);
+const Ellie = new Aluno("Ellie", 5);
+const Emma = new Aluno("Emma", 6);
+
+const alunos = [Pedro, Adam, Tim, Ellie, Emma];
+
+function alunosAprovados() {
+  const aprovados = alunos.filter((aluno) => aluno.nota >= 6);
+  console.log(aprovados);
+}
+
+alunosAprovados();
