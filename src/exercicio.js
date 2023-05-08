0790e6b35ec4cf35ede94bf323216673b469cba8
Paola Freitas <pahnetone@Paolas-MacBook-Air.local>
2023-05-08 14:34:50 -0300

    initial commit

---
+++ src/exercicio.js
@@ -0,0 +1,7 @@
+"use strict";
+function multiply(a, b) {
+    return a * b;
+}
+function sayHi(nome) {
+    return `Olá ${nome}`;
+}
---
+++ src/exercicio.ts
@@ -0,0 +1,7 @@
+function multiply(a: number, b: number): number {
+  return a * b;
+}
+
+function sayHi(nome: string): string {
+  return `Olá ${nome}`;
+}
